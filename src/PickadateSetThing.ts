1ecfd9756ce0f578612d4937af34fb641186176b
David Votrubec <david.votrubec@gmail.com>
2014-04-16 08:20:22 +0200

    Added helper object PickadateSetThing.ts
    String values which can be used for the 'set' method on pickadate instance.
    See http://amsul.ca/pickadate.js/api.htm#method-set for more info

---
+++ src/PickadateSetThing.ts
@@ -0,0 +1,17 @@
+/// <reference path="../interfaces/IPickadate.d.ts" />
+
+module Pickadate {
+
+    //based on http://amsul.ca/pickadate.js/api.htm#method-set
+    export var pickadateSetThing = {
+        clear: 'clear',
+        select: 'select',
+        highlight : 'highlight',
+        view: 'view',
+        min : 'min',
+        max : 'max',
+        disable : 'disable',
+        enable : 'enable',
+        interval : 'interval' //(time picker only)
+    }
+}
